29480e1da50688bf097200c1f38b777dc9372f32
Jeroen Ooms <jeroenooms@gmail.com>
2020-09-30 17:42:31 +0200

    test ps pkg

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,7 +3,7 @@ library(av)
 
 if (ps::ps_is_supported()) {
   # This sometimes has false positives on MacOS https://github.com/r-lib/ps/issues/90
-  reporter <- ps::CleanupReporter(testthat::CheckReporter)$new(proc_cleanup = FALSE, proc_fail = FALSE, conn_fail = FALSE)
+  reporter <- ps::CleanupReporter(testthat::CheckReporter)$new(proc_cleanup = FALSE, proc_fail = FALSE)
   test_check("av", reporter = reporter)
 } else {
   test_check("av")
